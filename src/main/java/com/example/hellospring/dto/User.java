4417d89cd3d0da9113a0115b56f670a138dc5f02
jabell0310 <jabell0310@naver.com>
2024-05-14 01:41:42 +0900

    feat: add DTO object named User

---
+++ src/main/java/com/example/hellospring/dto/User.java
@@ -0,0 +1,17 @@
+package com.example.hellospring.dto;
+
+
+
+public class User {
+
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+
+}
