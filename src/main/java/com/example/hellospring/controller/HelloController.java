c5dfa67c877111a4cfbd93eeae23c4ebfaae8cab
jabell0310 <jabell0310@naver.com>
2024-05-14 01:20:43 +0900

    Initial commit

---
+++ src/main/java/com/example/hellospring/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @ResponseBody
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello world";
+    }
+
+    @GetMapping("/hello2")
+    public String hello2() {
+        return "hello";
+    }
+}
