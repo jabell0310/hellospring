52d06441ffe6b2f32ed5bfd26bf5649fcebfe00c
jabell0310 <jabell0310@naver.com>
2024-05-14 01:37:57 +0900

    feat: add IntroduceController using with query parameter

---
+++ src/main/java/com/example/hellospring/controller/IntroduceController.java
@@ -0,0 +1,18 @@
+package com.example.hellospring.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class IntroduceController {
+
+    @GetMapping("/introduce")
+    public String introduce(@RequestParam(name="name", required = false, defaultValue = "김민준") String name, Model model) {
+        model.addAttribute("name", name);
+        return "introduce";
+    }
+
+}
